fbf0e85d641f2f85d3bc7a8d720e279b93f36add
Nigel <m0cvo@yahoo.co.uk>
2022-11-21 09:56:35 +0000

    Added page explaining how to add pictures and videos.

---
+++ Pages/Page2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET.CORE.WEB.DEMO.Pages
+{
+    public class Page2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
