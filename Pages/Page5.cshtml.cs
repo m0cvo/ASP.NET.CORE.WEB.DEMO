13ff870477232cdf3849fb49f8d37b71d4968ed8
Nigel <m0cvo@yahoo.co.uk>
2022-11-23 08:03:39 +0000

    Adding a page to demonstrate data binding with Blazor.

---
+++ Pages/Page5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET.CORE.WEB.DEMO.Pages
+{
+    public class Page5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
